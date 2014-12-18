1e5f67260097b75eb1c57ad375d6c0701053ff17
Hisashi HATAKEYAMA <me@hatak.jp>
2014-12-18 11:02:41 +0900

    fix

--- highlighter.py
+++ highlighter.py
@@ -12,21 +12,36 @@ from pygments.util import ClassNotFound
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+
 @app.route('/')
 def top():
     return render_template('top.html', lexers=get_all_lexers())
 
-@app.route('/dynamic/preview', methods=['POST'])
+
+@app.route('/api/preview', methods=['POST'])
 def preview():
-    binary = _gen(request.form['text'], font=request.form['font'], lexer=request.form['lexer'])
+    try:
+        lexer = get_lexer_by_name(request.form['lexer'])
+    except ClassNotFound:
+        lexer = TextLexer()
+
+    kwargs = {
+        'font_size': request.form['font_size'],
+    }
+    if not 'is_display_linenum' in request.form:
+        kwargs['line_numbers'] = False
+
+    binary = _generate(request.form['text'], lexer, kwargs)
     return Response(binary.encode("base64"), mimetype='text/plain')
 
-@app.route('/dynamic/generate', methods=['POST'])
-def generate():
-    binary = _gen(request.form['text'], font=request.form['font'], lexer=request.form['lexer'])
-    return Response(binary, mimetype='image/png')
 
-@app.route('/dynamic/detect', methods=['POST'])
+# @app.route('/dynamic/generate', methods=['POST'])
+#def generate():
+#    binary = _generate(request.form['text'], font=request.form['font'], lexer=request.form['lexer'])
+#    return Response(binary, mimetype='image/png')
+
+
+@app.route('/api/detect', methods=['POST'])
 def detect_from_name():
     try:
         lexer = get_lexer_for_filename(request.form['name'])
@@ -34,16 +49,26 @@ def detect_from_name():
         lexer = TextLexer()
     return Response(lexer.aliases[0], mimetype='text/plain')
 
-def _gen(text, **kwargs):
-    try:
-        lexer = get_lexer_by_name(kwargs['lexer'])
-    except ClassNotFound:
-        lexer = TextLexer()
 
-    f = ImageFormatter(font_name=kwargs['font'])
-    sio = StringIO(highlight(text, lexer, f))
+def _generate(text, lexer, kwargs):
+    convert_text = []
+
+    # check multi-byte character and append dummy character(space)
+    for line in text.split('\n'):
+        count = 0
+        for c in line:
+            if ord(c) > 255:
+                count += 1
+        convert_text.append(line + str(" " * count))
+
+    # Font: RictyDiscord-Regular"
+    # Style: EclipseStyle, emacs, default,
+    f = ImageFormatter(font_name="RictyDiscord-Regular",
+                       line_pad=5, line_number_bg="#fff", style="github", **kwargs)
+    sio = StringIO(highlight("\n".join(convert_text), lexer, f))
     sio.seek(0)
     return sio.getvalue()
+
 
 if __name__ == '__main__':
     app.run()
--- highlighter/_mapping.py
+++ highlighter/_mapping.py
@@ -1,6 +1,5 @@
 # 'FontName': ('font filename', is_include_japanese)
 FONTS = {
     'ArialFont': ('Arial', 'Arial', False),
-    'RictyFont': ('Ricty-Regular', 'Ricty', True),
-    'SourceCodeProFont': ('SourceCodePro-Regular', 'SourceCode Pro', False)
-    }
+    'Ubuntu Mono Regular': ('UbuntuMono-R', False),
+}
